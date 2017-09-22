994cb68050e4547690699cf4060c8cf6dceaaaba
myje5989 <30823860+myje5989@users.noreply.github.com>
2017-09-22 09:57:04 +0700

    Initial commit

---
+++ src/observer2/IObserver.java
@@ -0,0 +1,5 @@
+package observer2;
+
+public interface IObserver {
+	 void update(int i);
+}
---
+++ src/observer2/ISubject.java
@@ -0,0 +1,8 @@
+package observer2;
+
+public interface ISubject {
+	 void register(IObserver o);        
+	 void unregister(IObserver o);        
+	 void notifyObservers(int updatedValue); 
+
+}
